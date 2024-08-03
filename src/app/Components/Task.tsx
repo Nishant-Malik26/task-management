485feb633b3b14489aa0368e39a9523cb7602272
Nishant Malik <nishantmalik2015@gmail.com>
2024-08-03 11:42:20 +0530

    changed ui

--- src/app/Components/Task.tsx
+++ src/app/Components/Task.tsx
@@ -1,25 +1,30 @@
 "use client";
-import { Draggable } from "@hello-pangea/dnd";
 import React from "react";
-
+import dayjs from "dayjs";
 const Task = ({
   _id: id,
-  // index,
   title,
   status,
   priority,
   deadline,
   date,
   addedProperties,
-  _id,
 }: any) => {
   return (
-    <div className="bg-[#F8F9FA] border-2	border-black-50	mt-2 p-3">
-      <div>{title || ""}</div>
-      <div>{status || ""}</div>
-      <div>{priority || ""}</div>
-      <div>{date || ""}</div>
-      <div>{deadline || ""}</div>
+    <div className="bg-[#F8F9FA] border-2 rounded-xl	border-black-50	mt-2 p-3">
+      <div className="text-black-700 font-semibold">{title || ""}</div>
+      <div className="text-gray-700 font-thin">{status || ""}</div>
+      {priority && (
+        <div className="rounded-sm w-max border-2 border-solid border-red-600 bg-red-600 p-1 text-white">
+          {priority}
+        </div>
+      )}
+      <div>{dayjs(deadline)?.format("DD MMM YYYY")}</div>
+      <div className="text-gray-500">{dayjs(date)?.format("DD MMM YYYY")}</div>
+      {addedProperties.length > 0 &&
+        addedProperties.map((prop: any) => (
+          <span key={prop.id}>{prop.val}</span>
+        ))}
     </div>
   );
 };
